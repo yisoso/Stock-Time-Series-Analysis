7c013046fe2b7008368ff7ba19ba80eee8f2451d
yisoso <self_reg@qq.com>
2019-11-17 11:50:23 +0800

    Revert "add python code"
    This reverts commit 0d78999ef31a3d8e7bccc9d142f1adebc704c50d.

--- python/read_data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-ibm_data_path = 'F:\pythonProject\Stock-Time-Series-Analysis\data\IBM_2006-01-01_to_2018-01-01.csv'
-data = pd.read_csv(ibm_data_path)
-
-# 'Close' is used as the analysis objection
-cl_time = data['Close']
